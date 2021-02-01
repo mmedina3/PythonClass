fbc98b337367297c881b0949aa0de0bd3e1576ea
mmedina3 <medina.m1016@gmail.com>
2021-01-31 21:21:32 -0800

    Split and reverse word, discussion 8

---
+++ discussion8.py
@@ -0,0 +1,7 @@
+# Write one example of a string function, and briefly explain what the function does.
+# Here is an example:
+# #The len function takes a string and returns the number of characters in that string.
+word = input("Insert a word: ")
+word_split = word.split()
+word_reversed = "".join(reversed(word))
+print(word_reversed) 
