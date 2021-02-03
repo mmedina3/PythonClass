4a60fe793574c6e0e53eb91dac26c0d03ad45fd3
mmedina3 <medina.m1016@gmail.com>
2021-02-02 19:43:04 -0800

    discussion 9

---
+++ discussion9.py
@@ -0,0 +1,11 @@
+# Post an example of code that operates on a list or tuple.
+# Examples: slicing a list, slicing from the end, or the beginning, checking if one list is included in another list, copying a list, reversing a list.
+
+#Join lists and sort them
+
+fruits = ['Orange', 'Apple', 'Watermelon']
+veggies = ['Potato', 'Carrots', 'Squash' ]
+
+join_lists = fruits + veggies
+join_lists.sort()
+print(join_lists)
